e783f4ae8974dc3ba51d8320c6a22589864a0344
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-10 22:03:26 +0200

    remove autogenerated comments

--- lib/reprise.ex
+++ lib/reprise.ex
@@ -1,18 +1,13 @@
 defmodule Reprise do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
       worker(Reprise.Server, [[interval: 1000]])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Reprise.Supervisor]
     Supervisor.start_link(children, opts)
   end
