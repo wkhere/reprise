6c0e8b80656430358653abb14ce573253e784c41
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-21 13:03:18 +0200

    some tests on beam/mod retrieval

--- test/reprise_test.exs
+++ test/reprise_test.exs
@@ -1,7 +1,21 @@
 defmodule RepriseTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "my own beams have proper names" do
+    for b <- Reprise.Runner.beams,
+      f = b |> Path.split |> List.last,
+      do:
+        assert f =~ ~r/^Elixir\.Reprise\./
   end
+
+  test "can find my own beam files" do
+    for b <- Reprise.Runner.beams, do:
+      assert File.regular? b
+  end
+
+  test "can find my modules via beam files" do
+    for {_f,m} <- Reprise.Runner.beam_modules, do:
+      assert "#{m}" =~ ~r/^Elixir\.Reprise/
+  end
+
 end
