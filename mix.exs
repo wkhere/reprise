1d6e6197b273461f9e9172712288fc88146b8e22
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-21 13:38:10 +0200

    bump patchlvl

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Reprise.Mixfile do
 
   def project do
     [app: :reprise,
-     version: "0.2.1",
+     version: "0.2.2",
      elixir: "~> 0.14.2",
      description: description,
      package: package,
