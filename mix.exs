59f3e10f1a385a10a3e06d57d563f1a24553b406
Wojtek Kaczmarek <wojtek@yexperiment.com>
2015-12-17 19:46:09 +0100

    dev ver [ci skip]

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Reprise.Mixfile do
 
   def project do
     [app: :reprise,
-     version: "0.4.3",
+     version: "0.4.4-dev",
      elixir: "~> 1.0",
      description: description,
      package: package,
